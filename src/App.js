018699af08d3d2dcb3db734d8c62cc174b58ed53
Sett Wai Aung <settwaiaung@gmail.com>
2019-12-15 15:43:06 +0630

    add add/remove function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 class Item extends React.Component {
   render() {
     return (
-      <li>{this.props.name}</li>
+      <li>
+        {this.props.item.name}
+        <a href="#/" onClick={() => {
+          this.props.remove(this.props.item._id)
+        }}>&times;</a>
+      </li>
     )
   }
 }
@@ -13,17 +18,30 @@ class App extends React.Component {
   constructor () {
     super()
     this.state = {
-      data : ['Orange', 'Banana', 'Watermelon'],
+      data : [
+        {'_id' : 1, 'name' : 'Book'},
+        {'_id' : 2, 'name' : 'Pencil'},
+        {'_id' : 3, 'name' : 'Pen'},
+      ],
     }
-
+    this.autoid = this.state.data.length;
     this.input = React.createRef();
     this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
   }
 
   add() {
     var data = this.state.data;
     var name = this.input.current.value;
-    data.push(name);
+    data.push({'_id': ++this.autoid, 'name': name});
+    this.setState({
+      data: data,
+    })
+    this.input.current.value = ""
+  }
+
+  remove(_id) {
+    var data = this.state.data.filter(item => item._id !== _id);
     this.setState({
       data: data,
     })
@@ -34,8 +52,11 @@ class App extends React.Component {
       <div>
         <h1>Hello React</h1>
         <ul>
-          {this.state.data.map(name => (
-            <Item name={name} />
+          {this.state.data.map((item) => (
+            <Item 
+              item={item}
+              key={item._id} 
+              remove={this.remove} />
           ))}
         </ul>
         <input type="text" ref={this.input}/>
