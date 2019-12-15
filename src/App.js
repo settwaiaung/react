357c59dde474abad8fc6e246002bc0f53559abd1
Sett Wai Aung <settwaiaung@gmail.com>
2019-12-15 14:47:32 +0630

    React Basic Syntax

--- src/App.js
+++ src/App.js
@@ -3,38 +3,44 @@ import React from 'react';
 class Item extends React.Component {
   render() {
     return (
-    <li>{this.props.name}</li>
+      <li>{this.props.name}</li>
     )
   }
 }
 
 class App extends React.Component {
-  constructor() {
-    super();
+
+  constructor () {
+    super()
     this.state = {
-      data : ['Alex', 'Bob', 'John'],
+      data : ['Orange', 'Banana', 'Watermelon'],
     }
+
     this.input = React.createRef();
     this.add = this.add.bind(this);
   }
-  
+
   add() {
+    var data = this.state.data;
+    var name = this.input.current.value;
+    data.push(name);
     this.setState({
-      data: [...this.state.data, this.input.current.value],
+      data: data,
     })
   }
+
   render() {
     return (
       <div>
         <h1>Hello React</h1>
-        <p>USER NAME: KYAW GYI</p>
         <ul>
-          {this.state.data.map( i => {
-            return (<Item name={i}/>)
-          })}
+          {this.state.data.map(name => (
+            <Item name={name} />
+          ))}
         </ul>
-        <input type="text" ref={this.input}/><button onClick={this.add}>+</button>
-      </div> 
+        <input type="text" ref={this.input}/>
+        <button onClick={this.add}>Add</button>
+      </div>
     )
   }
 }
